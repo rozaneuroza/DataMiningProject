30e0b34b4ec7eec4a3920ada2553ab6072322b1f
Julia <juliarotko@gmail.com>
2026-04-18 13:04:18 +0200

    finish FE, random forest, start RNN

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 USAGE_VARS = ['call', 'sms', 'screen', 'appCat.builtin', 'appCat.communication', 'appCat.entertainment', 'appCat.finance', 'appCat.game', 'appCat.office', 'appCat.other', 'appCat.social', 'appCat.travel', 'appCat.unknown', 'appCat.utilities', 'appCat.weather']
 STATE_VARS = ["mood", "circumplex.arousal", "circumplex.valence", "activity"]
@@ -188,7 +194,41 @@ def sliding_window_features(df, windows=[3, 7, 14, 30]):
     
     return pd.concat([df, pd.DataFrame(new_cols, index=df.index)], axis=1)
 
+
+### CLASSIFICATION ###
+
+# We implement two types of algorithms
+# 1. Instance based algorithm: not a sequential, temporal prediction; makes a prediction indepdenetly based on the previous row (id,date)
+#    - The temporal nature is encoded in the features, e.g. sliding window features, daily variation, etc. (shows importance of feature engineering)
+# 2. Temporal model: utilizes the temporal nature of the data 
+#    - Learns also the sequential relationships 
+
+## 1. Instance based classification: Random Forest
+
+## 2. Temporal classification: Recurrent neural network
+
+def create_sequences(df, window_size=7):
+    # Creates sequences of size of window, with target - next day 
+
+    X = []
+    y = [] 
+
+    feature_columns = [c for c in df.columns if c not in ('id', 'date', 'mood_class', "mood_class_target")]
+
+    for uid, user_df in df.groupby('id'):
+        user_df = user_df.sort_values("date")
+        features = user_df[feature_columns].values
+        targets = user_df['mood_classs_target'].values
+
+        for i in range(len(user_df) - window_size):
+            X.append(features[i:i+window_size])
+            y.append(targets[i+window_size])
+
+    return np.array(X, dtype = np.float32), np.array(y)
+
+
 def main():
+    ### DATA CLEANING: TASK 1 ###
     # Load data:
 
     df = load_data(file_path = "dataset_mood_smartphone.csv")
@@ -234,9 +274,90 @@ def main():
 
     cols = ['id', 'date'] + [c for c in df.columns if c not in ('id', 'date')]
 
+    print("Final dataset with features + sliding windows:")
     print(df.head())
 
+    print("Investigate mood variable (normalized):")
+    print(df['mood'].describe())
+    print(df['mood'].head(20))
+
+    # Engineer classes for supervised learning: mood class
+    # Mood is an average mood during the day 
+    df['mood_class'] = pd.cut(df['mood'], bins=[-np.inf, 4,6,8, np.inf], labels=['low', 'medium', 'high', 'very_high'])
+    # We use up to 4 for low because there are no values below 2 to have a class "very_low"
+    print(df['mood_class'].value_counts())
+    print(df['mood_class'].head(10))
+
+    # Check missing values?
+    print(df.isnull().sum()[df.isnull().sum() > 0])
+    # Result: 27 NaN values in every variable - in the first row for each user diff() has nothing to subtract, so produces this
+    # We drop this row
+    diff_cols = [c for c in df.columns if '_diff_' in c]
+    df = df.dropna(subset=diff_cols)
+
+
     save_dataset(df, "data_with_features.csv")
+
+    ### CLASSIFICATION: TASK 2 ###
+
+    # 1. Instance based: Random Forest 
+    # 1. Train test split
+    X = df.drop(columns=['id', 'date', 'mood_class'])
+    y = df['mood_class']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    # 2. Fit Random Forest Classifier
+
+    rf = RandomForestClassifier(random_state=42)
+    rf.fit(X_train, y_train)
+
+    # Make a prediction 
+
+    y_pred = rf.predict(X_test)
+
+    # Evaluate performance 
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+    # Visualize first 3 decision trees 
+
+    for decision in range(3):
+        tree = rf.estimators_[decision]
+        plt.figure(figsize=(20,10))
+        plot_tree(tree, filled = True, feature_names=X.columns, class_names=rf.classes_, rounded=True, 
+              max_depth=3)
+        plt.savefig(f"tree_{decision}.png")
+        plt.close()
+
+    # 2. Temporal model: RNN 
+    
+    # We need to prepare the sequential data 
+    # Because the target is the next day's mood, we shift the mood class column by -1 (for supervised learning)
+        
+    df['mood_class_target'] = df.groupby('id')['mood_class'].shift(-1)
+    # We drop the last row for each user because it has no target
+    df = df.dropna(subset=['mood_class_target'])
+
+    X, y = create_sequences(df, window_size=7)
+
+    # Train test split (sequentially)
+    threshold = int(0.8 * len(X))
+    X_train, X_test = X[:threshold], X[threshold:]
+    y_train, y_test = y[:threshold], y[threshold:]
+
+    # Scale 
+    scaler = StandardScaler()
+    n_samples, window, n_features = X_train.shape
+    X_train = scaler.fit_transform(X_train.reshape(-1, n_features)).reshape(-1, window, n_features)
+    X_test = scaler.transform(X_test.reshape(-1, n_features)).reshape(-1, window, n_features)
+
+    # Change string classes into integers
+    classes = ['low', 'medium', 'high', 'very_high']
+    class_to_idx = {c: i for i, c in enumerate(classes)}
+    y_train = np.array([class_to_idx[c] for c in y_train])
+    y_test  = np.array([class_to_idx[c] for c in y_test])
 
 if __name__ == "__main__":
     main()
